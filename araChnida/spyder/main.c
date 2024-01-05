18d4e0584c117f88fa84bec18f0f0428492d4305
Cpt-Graby <83433380+Cpt-Graby@users.noreply.github.com>
2024-01-05 11:17:07 +0100

    Update main.c
    Update: discorvery of libcurl c

--- araChnida/spyder/main.c
+++ araChnida/spyder/main.c
@@ -1,33 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: agonelle <marvin@42lausanne.ch>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/14 17:16:42 by agonelle          #+#    #+#             */
-/*   Updated: 2023/10/14 18:01:22 by agonelle         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <curl/curl.h>
 
-int	main(void)
-{
-	CURL				*curl;
-	CURLcode			res;
+int     curl_easy_test(void);
 
-	curl = curl_easy_init();
-	if (!curl)
-		return (-1);
-	curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
-	res = curl_easy_perform(curl);
-	if (res != CURLE_OK)
-		return (-1);
-	curl_easy_cleanup(curl);
-	return (0);
+int     main(int argc, char **argv)
+{
+        (void) argc;
+        (void) argv;
+        int exit;
+
+
+        exit = curl_easy_test();
+        return(exit);
 }
-	//curl_easy_strerror(res);
-	// https://www.youtube.com/watch?v=KSc4zf5t6I4
+
+int curl_easy_test(void)
+{
+        CURL *curl = curl_easy_init();
+
+        if (!curl)
+        {
+                fprintf(stderr, "init failed!");
+                return (EXIT_FAILURE)
+        }
+
+        curl_easy_cleanup(curl);
+        return (EXIT_SUCCESS);
+}
