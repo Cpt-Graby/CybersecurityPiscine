9cac239de96714d0a9e26a625b721767101c7503
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2024-05-11 00:20:26 +0200

    Add: adding engine fonction for the recursivity of the programm

--- araChnida/spyder/spyder.py
+++ araChnida/spyder/spyder.py
@@ -7,24 +7,30 @@ import spyder_utils
 import simple_request
 
 
-def spyder_engine():
-    
-    # TODO: Checking of the input
-    # dest_url = 'https://www.upwork.com/resources/best-web-scraper'
-    # dest_url = 'https://www.google.com/'
-    test_img = 'https://img.lemde.fr/2024/04/25/0/0/6000/4000/180/0/95/0/26fd839_0cdba6be2ec245c8a17c215168705e19-0-7ce2120a106a4d36af120100ae287304.jpg'
-    dest_url = 'https://www.lemonde.fr'
-    response = simple_get(dest_url, True)
-    urls = making_soup(response.text)
-    for img_url in urls[1]:
-        print(img_url)
-        # download_img(img_url)
-    print(len(urls[1]))
+def spyder_engine(urls_to_check : set, count : int):
+    'All the engine of the spyder programm is here'
+    set_img = set()
+    current_url = ''
+    while (len(urls_to_check) >= 1 and count > 0):
+        dest_url = urls_to_check.pop()
+        response = simple_request.simple_get(dest_url, True)
+        urls = simple_request.making_soup(response.text)
+        set_img.update(urls[1])
+        urls_to_check.update(urls[0])
+        count -= 1
+
+    print(f'urls2check: {urls_to_check}')
+    #test_img = 'https://img.lemde.fr/2024/04/25/0/0/6000/4000/180/0/95/0/26fd839_0cdba6be2ec245c8a17c215168705e19-0-7ce2120a106a4d36af120100ae287304.jpg'
+    #for img_url in urls[1]:
+    #    print(img_url)
+    #    # download_img(img_url)
+    #print(len(urls[1]))
 
 
 def main():
     data_dir = f'{os.getcwd()}/data'
     recursive = False
+    depth = 5
     # Get args and handle them
     try:
         opts, args = getopt.getopt(sys.argv[1:], "p:")
@@ -47,7 +53,13 @@ def main():
     # The programm start to do the work
     print('------------')
     spyder_utils.creation_picture_folder(data_dir)
-    simple_request.spyder_engine(args[0], recursive)
+    all_url = set([args[0]])
+    if (recursive == False):
+        depth = 1
+    spyder_engine(all_url, depth)
+    print("----------")
+    print(f'test:: {all_url}')
+
 
 if __name__ == "__main__":
     main()
